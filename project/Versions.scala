074017e0d3e62e7a5577879818a497cf28e8e358
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-25 03:21:55 +0000

    SBT build specification.
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti@744 b05f3425-8805-43f2-8a1d-5852a8693556

---
+++ project/Versions.scala
@@ -0,0 +1,15 @@
+
+object Versions {
+  val scala = "2.11.6"
+
+  val xmlResolver = "1.2"
+
+  val emf_ecore = "2.10.1"
+
+  val graph_core = "1.9.1"
+  val graph_constrained = "1.9.0"
+  val graph_dot = "1.10.0"
+  val graph_json = "1.9.2"
+
+  val scalaz = "7.1.1"
+}
