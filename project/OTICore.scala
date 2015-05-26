d682b94503df67445ee56a02e63b0986ff0b8e55
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-05-26 13:33:26 +0000

    SVN-versioning
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti@752 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/OTICore.scala
+++ project/OTICore.scala
@@ -5,7 +5,6 @@ import com.typesafe.sbt.SbtGit._
 import net.virtualvoid.sbt.graph.Plugin.graphSettings
 import sbt.Keys._
 import sbt._
-import xerial.sbt.Pack._
 
 object OTICore extends Build {
 
@@ -43,34 +42,24 @@ object OTICore extends Build {
     file(".")).
     settings(otiSettings: _*).
     settings(commonSettings: _*).
-    settings(packSettings: _*).
-    settings(        
-        version := "0.12.0-" + {
-          val svnProc = Process(command="svn", arguments=Seq("info"))
-          val sedCommand="s/^.*Revision:[[:space:]]\\{1,\\}\\([[:digit:]]\\{1,\\}\\).*$/\\1/p"
-          val sedProc = Process(command="sed", arguments=Seq("-n", sedCommand))
-          val svnRevision = svnProc.#|(sedProc).!!
-          svnRevision
-        },
-        removeExistingHeaderBlock := true,
-        packExpandedClasspath := false,
-        packLibJars := Seq.empty,
-        packUpdateReports := Seq.empty,
-        libraryDependencies ++= Seq(
-          "org.scala-lang" % "scala-reflect" % Versions.scala % "provided" withSources() withJavadoc(),
-          "org.scala-lang" % "scala-library" % Versions.scala % "provided" withSources() withJavadoc(),
-          "org.scala-lang" % "scala-compiler" % Versions.scala % "provided" withSources() withJavadoc(),
-          "org.scalaz" %% "scalaz-core" % Versions.scalaz % "compile" withSources() withJavadoc(),
-          "xml-resolver" % "xml-resolver" % Versions.xmlResolver % "compile" withSources(),
-          "com.assembla.scala-incubator" %% "graph-core" % Versions.graph_core % "compile" withSources() withJavadoc(),
-          "com.assembla.scala-incubator" %% "graph-constrained" % Versions.graph_constrained % "compile" withSources() withJavadoc(),
-          "com.assembla.scala-incubator" %% "graph-json" % Versions.graph_json % "compile" withSources() withJavadoc(),
-          "com.assembla.scala-incubator" %% "graph-dot" % Versions.graph_dot % "compile" withSources() withJavadoc()
-        ),
-        scalaSource in Compile := baseDirectory.value / "src",
-        unmanagedSourceDirectories in Compile += baseDirectory.value / "src-gen",
-        classDirectory in Compile := baseDirectory.value / "bin",
-        shellPrompt := { state => Project.extract(state).currentRef.project + " @ " + Project.extract(state).get(GitKeys.gitCurrentBranch) + "> " }
+    settings(
+      version := Versions.version,
+      removeExistingHeaderBlock := true,
+      libraryDependencies ++= Seq(
+        "org.scala-lang" % "scala-reflect" % Versions.scala % "provided" withSources() withJavadoc(),
+        "org.scala-lang" % "scala-library" % Versions.scala % "provided" withSources() withJavadoc(),
+        "org.scala-lang" % "scala-compiler" % Versions.scala % "provided" withSources() withJavadoc(),
+        "org.scalaz" %% "scalaz-core" % Versions.scalaz % "compile" withSources() withJavadoc(),
+        "xml-resolver" % "xml-resolver" % Versions.xmlResolver % "compile" withSources(),
+        "com.assembla.scala-incubator" %% "graph-core" % Versions.graph_core % "compile" withSources() withJavadoc(),
+        "com.assembla.scala-incubator" %% "graph-constrained" % Versions.graph_constrained % "compile" withSources() withJavadoc(),
+        "com.assembla.scala-incubator" %% "graph-json" % Versions.graph_json % "compile" withSources() withJavadoc(),
+        "com.assembla.scala-incubator" %% "graph-dot" % Versions.graph_dot % "compile" withSources() withJavadoc()
+      ),
+      scalaSource in Compile := baseDirectory.value / "src",
+      unmanagedSourceDirectories in Compile += baseDirectory.value / "src-gen",
+      classDirectory in Compile := baseDirectory.value / "bin",
+      shellPrompt := { state => Project.extract(state).currentRef.project + " @ " + Versions.version_suffix + "> " }
     )
 
 }
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,37 +1,52 @@
 
+import sbt.Process
+
 // see JPLMBEECOmmonScalaLibraries.Version
 object Versions {
 
-    // scalaLibs
+  val version_prefix = "0.13.0"
+  val version_suffix = {
+    val svnProc = Process(command = "svn", arguments = Seq("info"))
+    val sedCommand = "s/^.*Revision:[[:space:]]\\{1,\\}\\([[:digit:]]\\{1,\\}\\).*$/\\1/p"
+    val sedProc = Process(command = "sed", arguments = Seq("-n", sedCommand))
+    val svnRevision = svnProc.#|(sedProc).!!.trim
+    svnRevision
+  }
 
-    val scala = "2.11.6"
-    val scala_suffix = "2.11"
+  // OTI Core version
 
-    // otherLibs
+  val version = version_prefix + "-" + version_suffix
 
-    val config = "1.2.1"
-    val scalaCheck = "1.12.2"
-    val scalaTest = "2.2.4"
-    val specs2 = "3.6"
-    val parboiled = "2.1.0"
-    val akka = "2.3.9"
-    val spray = "1.3.3"
-    val shapeless = "2.1.0"
-    val scalaz = "7.1.1"
-    val scalaz_stream = "0.7a"
-    val play = "2.4.0-RC5"
+  // scalaLibs
 
-    // owlapiLibs
+  val scala = "2.11.6"
+  val scala_suffix = "2.11"
 
-    /** @see http://mvnrepository.com/artifact/net.sourceforge.owlapi/owlapi-distribution */
-    val owlapi = "4.0.1"
+  // otherLibs
 
-    /** @see http://mvnrepository.com/artifact/xml-resolver/xml-resolver/ */
-    val xmlResolver = "1.2"
+  val config = "1.2.1"
+  val scalaCheck = "1.12.2"
+  val scalaTest = "2.2.4"
+  val specs2 = "3.6"
+  val parboiled = "2.1.0"
+  val akka = "2.3.9"
+  val spray = "1.3.3"
+  val shapeless = "2.1.0"
+  val scalaz = "7.1.1"
+  val scalaz_stream = "0.7a"
+  val play = "2.4.0-RC5"
 
-    // graphLibs
-    val graph_core = "1.9.1"
-    val graph_constrained = "1.9.0"
-    val graph_dot = "1.10.0"
-    val graph_json = "1.9.2"
+  // owlapiLibs
+
+  /** @see http://mvnrepository.com/artifact/net.sourceforge.owlapi/owlapi-distribution */
+  val owlapi = "4.0.1"
+
+  /** @see http://mvnrepository.com/artifact/xml-resolver/xml-resolver/ */
+  val xmlResolver = "1.2"
+
+  // graphLibs
+  val graph_core = "1.9.1"
+  val graph_constrained = "1.9.0"
+  val graph_dot = "1.10.0"
+  val graph_json = "1.9.2"
 }
