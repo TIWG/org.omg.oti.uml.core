47e84ce83c74d99e39ca64fbf677a1ba8542492d
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-12-14 15:49:35 +0000

    OTI Document API update based on discussions at the OMG TC meetings in La Jolla.
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti@445320 b05f3425-8805-43f2-8a1d-5852a8693556

--- src/org/omg/oti/uml/xmi/Document.scala
+++ src/org/omg/oti/uml/xmi/Document.scala
@@ -39,45 +39,79 @@
  */
 package org.omg.oti.uml.xmi
 
-import java.net.URI
 import org.omg.oti.uml.RelationTriple
 import org.omg.oti.uml.characteristics.OTISpecificationRootCharacteristics
 import org.omg.oti.uml.read.api.{UML, UMLElement, UMLPackage}
 import org.omg.oti.uml.read.operations.UMLOps
 
-import scala.collection.immutable.Set
+import scala.collection.immutable._
+import scala.Enumeration
+
+object DocumentKind extends Enumeration {
+  type DocumentKind = Value
+  val BuiltInImmutable, BuiltInMutable, SerializableImmutable, SerializableMutable = Value
+  
+  def isImmutable(kind: DocumentKind): Boolean =
+    kind == BuiltInImmutable || kind == SerializableImmutable
+      
+  def isSerializable(kind: DocumentKind): Boolean =
+    kind == SerializableImmutable || kind == SerializableMutable
+    
+}
+
+import DocumentKind._
 
 /**
- * Corresponds to OMG XMI 2.5.1 Document combined with OMG MOF 2.5 Extent.
- *
- * The OMG XMI specification defines the mapping of MOF models to XMI Documents.
- *
- * Although the OMG XMI specification refers to several important characteristics of XMI Documents;
- * there is surprisingly no abstract syntax specification for what an XMI Document is.
- *
- * Although the OMG XMI specification defines the mapping between MOF models and XMI Documents,
- * the specification does not address the practical aspects of managing such mappings.
- * In principle, the OMG XMI specification allows multiple mappings from the same MOF model to multiple XMI Documents.
- * In practice, most implementations of OMG UML 2.5 assume a 1-1 mapping between a MOF model and an XMI Document.
- *
- * The OTI concept of XMI Document reflects a 1-1 mapping between a MOF model and an XMI Document.
- * The MOF model associated to an XMI Document is specified in terms of the root element `scope`.
- * To simplify matters of inter-document linking (see OMG XMI 2.5.1, section 7.10), an OTI XMI Document
- * includes a `documentURL` to specify the external URI where the document is published and the `uri` of the root element.
- *
- * Notes:
- *   - OMG XMI 2.5.1, formal/15-06-07 lacks a definition of XMI Document and of XMI Schema
- *   - OMG XMI 2.5, ptc/14-09-21, Section 4 defines the following:
- *
- *      > [XMIDocument] A document produced by the XMI production rules defined in this International Standard.
- *      > [XMISchema] A schema produced by the XMI production rules defined in this International Standard.
- *
- * @todo Consider restricting the type of `scope` from UMLElement to UMLPackage.
- *       In principle, the OMG XMI specification allows an arbitrary element to be the root of an XMI Document.
- *       However, it is unclear how this works... In practice, the OTI effectively requires the root to be a UMLPackage.
- *
- * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
- */
+  * An OTI Document corresponds to an OMG MOF 2.5.1 Extent and provides support OTI Document services.
+  *
+  * OTI Document-related services include, but are not limited to:
+  * - serialization/export/save & deserialization/import/load, 
+  * - inter-document cross-reference analysis for managing graphs of documents (acyclic or circular)
+  * - preventing update operations on elements in immutable documents
+  * 
+  * The topology of cross-references amongst OTI Documents (mutable or not) is an important
+  * consideration for the safety of loading/saving OTI Documents in general.
+  * 
+  * These operations are simpler when the topology is acyclic because the OTI Documents
+  * can be ordered to ensure that loading or saving an OTI Document happens
+  * after loading or saving the other cross-referenced OTI Documents.
+  * In contrast, loading/saving OTI Documents with circular cross-references requires special handling
+  * for the case where a cross-reference refers to an OTI Document that has not yet been loaded/saved.
+  *
+  * These operations are more complex when the topology is circular or when OTI Documents are mutable.
+  * Although the OMG XMI specification defines the mapping between MOF models and XMI Documents,
+  * the specification does not address several important practical considerations, such as:
+  * - no abstract syntax for the information necessary for the serialization of OMG MOF Extents
+  *   (e.g., xmi:id, xmi:uuid)
+  * - no abstract syntax for the information necessary for the identification of serialized XMI Documents
+  *   (e.g., document URL, namespace prefix)
+  * - unclear relationship between OMG MOF Extent and the scope of export (serialization) in OMG XMI 2.5.1
+  * - no process for importing (deserializing) an XMI Document into an OMG MOF Extent
+  *   (the OMG XMI specification defines the export (serialization) of an OMG MOF Extent to an XMI Document)
+  *
+  * Notes:
+  * - OMG XMI 2.5.1, formal/15-06-07 lacks a definition of XMI Document and of XMI Schema
+  * - OMG XMI 2.5, ptc/14-09-21, Section 4 defines the following:
+  *
+  *      > [XMIDocument] A document produced by the XMI production rules defined in this International Standard.
+  *      > [XMISchema] A schema produced by the XMI production rules defined in this International Standard.
+  *
+  * - OMG XMI 2.5.1 Section 9.1 states:
+  *      > This clause specifies the XMI production of an XML document from a model based on the MOF 2 Core. 
+  *   
+  *      Unfortunately, it is unclear what "a model based on the MOF 2 Core" actually is.
+  *      Is an model element that is an instance of any concrete MOF metaclass a legitimate "model"?
+  *  
+  * OTI reflects a deliberate choice to restrict the use of OMG specifications in the following way:
+  * - an OTI Document is scoped by a UML Package
+  *   That is, OMG XMI 2.5.1 section 9 is effectively restricted to serializing the scope of a UML Package
+  *   as the root of the XML Document produced. This choice reflects the fact that the UML Package::URI
+  *   plays an important role for the identification of elements across documents.
+  * - an OTI Document Package Scope corresponds to an OMG MOF 2.5.1 Extent, which is the basis
+  *   for identifying elements within it.
+  *
+  * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
+  */
 abstract trait Document[Uml <: UML] {
   implicit val ops: UMLOps[Uml]
 
@@ -86,6 +120,8 @@ abstract trait Document[Uml <: UML] {
 
   val info: OTISpecificationRootCharacteristics
 
+  def kind: DocumentKind
+  
   /**
    * The location where this document is published as
    * a loadable resource that can be referenced by other documents
@@ -104,11 +140,47 @@ abstract trait Document[Uml <: UML] {
   val scope: UMLPackage[Uml]
 
   /**
+    * @param e a UML Element
+    * @return True if `e` is within the extent of this Document
+    */
+  def includes(e: UMLElement[Uml]): Boolean
+  
+  def extent: scala.collection.Iterable[UMLElement[Uml]]
+}
+
+/**
+  * A MutableDocument corresponds to an OMG MOF 2.5.1 Extent that is subject to change.
+  * 
+  * It is currently outside the scope of the OTI to ensure that load/save operations
+  * involving OTI MutableDocuments are serialized with respect to OTI model update operations.
+  *
+  * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
+  */
+abstract trait MutableDocument[Uml <: UML] 
+extends Document[Uml] {
+  
+  override def includes(e: UMLElement[Uml]): Boolean =
+    extent.find(_ == e).isDefined
+    
+}
+
+/**
+  * An ImmutableDocument corresponds to an OMG MOF 2.5.1 Extent that cannot change.
+  *
+  * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
+  */
+abstract trait ImmutableDocument[Uml <: UML] 
+extends Document[Uml] {
+  
+  override def includes(e: UMLElement[Uml]): Boolean =
+    extent.contains(e)
+    
+  /**
    * The set of all elements directly or indirectly owned by `scope`, including `scope`
    *
    * @return The reflexive, transitive closure of containment from `scope`
    */
-  def extent: Set[UMLElement[Uml]]
+  override val extent: Set[UMLElement[Uml]]
 }
 
 /**
@@ -119,24 +191,43 @@ abstract trait Document[Uml <: UML] {
  *
  * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
  */
-trait BuiltInDocument[Uml <: UML] extends Document[Uml] {
-  /**
-   * The extent of a built-in document, which is a tool-specific representation of a published document.
-   * This extent may be a partial representation of the full extent of the published document.
-   */
-  val builtInExtent: Set[UMLElement[Uml]]
-
-  override def extent = builtInExtent
-}
+abstract trait BuiltInDocument
 
 /**
  * A kind of Document whose extent is completely represented.
  *
  * @tparam Uml The type signature for a tool-specific adaptation of the OTI UML API
  */
-trait SerializableDocument[Uml <: UML] extends Document[Uml] {
-   
-  override lazy val extent: Set[UMLElement[Uml]] = 
-    Set(scope) ++ scope.allOwnedElements
+abstract trait SerializableDocument
 
- }
+trait BuiltInImmutableDocument[Uml <: UML]
+extends ImmutableDocument[Uml]
+with BuiltInDocument {
+   
+  override val kind: DocumentKind = BuiltInImmutable
+
+}
+
+trait BuiltInMutableDocument[Uml <: UML]
+extends MutableDocument[Uml]
+with BuiltInDocument {
+   
+  override val kind: DocumentKind = BuiltInMutable
+
+}
+
+trait SerializableImmutableDocument[Uml <: UML] 
+extends ImmutableDocument[Uml]
+with SerializableDocument {
+   
+  override val kind: DocumentKind = SerializableImmutable
+
+}
+
+trait SerializableMutableDocument[Uml <: UML] 
+extends MutableDocument[Uml]
+with SerializableDocument {
+   
+  override val kind: DocumentKind = SerializableMutable
+
+}
